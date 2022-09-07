8058522341eda36c254fe0a02e8f2c8235fc2013
Laxeder <Laxederbr@gmail.com>
2022-09-07 14:56:06 -0300

    Melhorando sistema de mensagem

--- src/domain/Message.ts
+++ src/domain/Message.ts
@@ -2,23 +2,30 @@ import BaseMessage from "../infrastructure/bot/BaseMessage";
 import Chat from "../infrastructure/bot/Chat";
 
 export default class Message implements BaseMessage {
+  private originalMention: any;
+
+  public mentions: string[] = [];
+  public mention?: Message;
+  public fromMe?: boolean;
   public isOld?: boolean;
-  public mention?: any;
+  public member?: string;
   public text: string;
+  public id?: string;
   public chat: Chat;
 
-  constructor(chat: Chat, text: string, mention?: any, isOld?: boolean) {
+  constructor(chat: Chat, text: string, mention?: Message, id?: string) {
     this.text = text;
     this.chat = chat;
+
     if (mention) this.mention = mention;
-    if (isOld) this.isOld = isOld;
+    if (id) this.id = id;
   }
 
   /**
    * * Define a sala de bate-papo
    * @param chat
    */
-  setChat(chat: Chat) {
+  public setChat(chat: Chat) {
     this.chat = chat;
   }
 
@@ -27,7 +34,7 @@ export default class Message implements BaseMessage {
    * @param text
    * @returns
    */
-  setText(text: string) {
+  public setText(text: string) {
     this.text = text;
   }
 
@@ -36,15 +43,63 @@ export default class Message implements BaseMessage {
    * @param mention
    * @returns
    */
-  setMention(mention: any) {
+  public setMention(mention: any) {
     this.mention = mention;
+  }
+
+  /**
+   * * Define se a mensagem é antiga
+   * @param isOld
+   */
+  public setIsOld(isOld: boolean) {
+    this.isOld = isOld;
+  }
+
+  /**
+   * * Define o ID da mensagem
+   * @param id
+   */
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  /**
+   * * Define se a mensagem foi enviada pelo bot
+   * @param fromMe
+   */
+  public setfromMe(fromMe: boolean) {
+    this.fromMe = fromMe;
+  }
+
+  /**
+   * * Define um membro da mensagem
+   * @param member
+   */
+  public setMember(member: string) {
+    this.member = member;
+  }
+
+  /**
+   * * Define as menções feitas nas mensagens
+   * @param mentions
+   */
+  public setMentions(mentions: string[]) {
+    this.mentions = mentions;
+  }
+
+  /**
+   * * Adiciona um numero a lista de mencionados
+   * @param mentionedId
+   */
+  public addMentioned(mentionedId: string) {
+    this.mentions.push(mentionedId);
   }
 
   /**
    * * Obter a sala de bate-papo da mensagem
    * @returns
    */
-  getChat(): Chat {
+  public getChat(): Chat {
     return this.chat;
   }
 
@@ -52,15 +107,56 @@ export default class Message implements BaseMessage {
    * * Obter o texto da mensagem
    * @returns
    */
-  getText(): string {
+  public getText(): string {
     return this.text;
   }
 
   /**
-   * * Obter a menção da mensagemf
+   * * Obter a menção da mensagem
    * @returns
    */
-  getMention(): any {
+  public getMention(): Message | undefined {
     return this.mention;
+  }
+
+  /**
+   * * retorna se a mensagem é antiga
+   * @returns
+   */
+  public getIsOld(): boolean | undefined {
+    return this.isOld;
+  }
+
+  /**
+   * * Retorna o ID da mensagem
+   * @returns
+   */
+  public getId(): string | undefined {
+    return this.id;
+  }
+
+  /**
+   * * retorna se
+   * @returns
+   */
+  public getFromMe(): boolean {
+    return this.fromMe || false;
+  }
+
+  //? O Baileys (WhatsApp) precisa da menção original para mencionar uma mensagem
+  /**
+   * * Define uma menção não refactorada
+   * @param originalMention
+   */
+  public setOriginalMention(originalMention: string): void {
+    this.originalMention = originalMention;
+  }
+
+  /**
+   * * Retorna a menção não refatorada
+   * @returns
+   */
+  public getOriginalMention(): any {
+    return this.originalMention;
   }
 }
--- src/infrastructure/bot/Chat.ts
+++ src/infrastructure/bot/Chat.ts
@@ -3,41 +3,62 @@ export interface Chats {
 }
 
 export default class Chat {
-  public members: string[] | Array<string> = [];
-  public fromMe?: boolean;
-  public member?: string;
-  public chatID?: string;
-  public isNew?: boolean;
-  public name?: string;
   public id: string;
+  public name?: string;
+  public isNew?: boolean;
 
   constructor(id: string, name?: string, isNew?: boolean) {
     this.id = id;
+
     if (name) this.name = name;
     if (isNew) this.isNew = isNew;
   }
 
+  /**
+   * * Define o ID da sala de bate-papo
+   * @param id
+   */
   public setId(id: string) {
     this.id = id;
   }
 
+  /**
+   * * Define o nome da sala de bate-papo
+   * @param name
+   */
   public setName(name: string) {
     this.name = name;
   }
 
-  public setMember(member: string) {
-    this.member = member;
+  /**
+   * * Define se é uma nova sala de bate-papo
+   * @param isNew
+   */
+  public setIsNew(isNew: boolean) {
+    this.isNew = isNew;
   }
 
-  public setMembers(members: string[] | Array<string>) {
-    this.members = members;
-  }
-
+  /**
+   * * Retorna o ID da sala de bate-papo
+   * @returns
+   */
   public getId(): string {
     return this.id;
   }
 
+  /**
+   * * Retorna o nome da sala de bate-papo
+   * @returns
+   */
   public getName(): string | undefined {
     return this.name;
+  }
+
+  /**
+   * * Retorna se é uma nova sala de bate-papo
+   * @returns
+   */
+  public getIsNew(): boolean {
+    return this.isNew || false;
   }
 }
